b6615ffeedda656d59eb35fa3ce676925dbbb23c
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-03-06 18:08:41 +0100

    styled components added

---
+++ src/globalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    
+  }
+`;
