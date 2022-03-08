077a4ff3eed14b4fb07d0c8a81331ef198554a81
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-08 20:20:46 +0100

    poppins font added to html, margin added to body

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,13 +3,13 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
-  }
+    font-family: 'Poppins', sans-serif;  }
 
   *, ::after, ::before {
     box-sizing: inherit;
   }
 
   body {
-    
+    margin: 0;
   }
 `;
