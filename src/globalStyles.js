9d98a92d8a0075b08661afc6438081d9f45df346
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-03-10 02:01:57 +0100

    google fonts back in index.html due to globalStyles warning

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
-
   html {
     box-sizing: border-box;
     font-family: 'Poppins', sans-serif; 
