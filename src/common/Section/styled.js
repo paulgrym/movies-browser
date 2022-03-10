f2b7103c89eab08a8f19d64244294302742298c9
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-03-10 01:24:26 +0100

    Section component structure changed, media queries added

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,21 +1,24 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
-    max-width: 1368px;
-    margin: 0 auto;
-    padding: 0 24px;
+  max-width: 1368px;
+  margin: 0 auto;
+  padding: 0 24px;
 `;
 
 export const Title = styled.h2`
-    color: ${({ theme }) => theme.colors.black};
-    font-size: 36px;
-    margin: 0;
-    margin-bottom: 24px;
-`;
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 36px;
+  line-height: 1.2;
+  margin: 24px 0;
 
-export const Body = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 208px);
-    grid-gap: 24px;
-    margin-bottom: 64px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}){
+    font-size: 28px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}){
+    font-size: 18px;
+    margin: 12px 0;
+  }
+    
 `;
