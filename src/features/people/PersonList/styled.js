bce9f6d39d41ed84d91b56cd06f204eab88dc544
Robert Osial <rob.osial@gmail.com>
2022-03-11 15:30:33 +0000

    ceated PersonList Component with styles

---
+++ src/features/people/PersonList/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 208px);
+  grid-gap: 16px;
+  justify-content: center;
+  margin: 12px auto 40px;
+
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}){
+    grid-template-columns: repeat(auto-fill, 136px);
+  }
+  `;
+
