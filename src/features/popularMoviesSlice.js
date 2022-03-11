02a531c8ba69ebc61b5f1842dcdc95bc63e87e7c
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-11 10:19:57 +0100

    moviesSlice created

---
+++ src/features/popularMoviesSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice ({
+  name: "popularMovies",
+  initialState: {
+    page: null,
+    movies: [],
+    totalPages: null,
+    totalResults: null,
+    loading: false,
+    error: false,
+  },
+
+  reducers: {
+    fetchMovies: (state) => {
+      state.loading = true;
+    },
+
+    fetchMoviesSuccess: (state, { payload: movies }) => {
+      state.loading = false;
+      state.error = false
+      state.page = movies.page;
+      state.movies = movies.results;
+      state.totalPages = movies.total_pages;
+      state.totalResults = movies.total_results;
+    },
+
+    fetchMovieError: (state) => {
+      state.error = true;
+    },
+  },
+});
+
+export const { 
+  fetchMovies,
+  fetchMoviesSuccess,
+  fetchMovieError } = moviesSlice.actions;
+
+const selectPopularMovies = (state) => state.popularMovies;
+
+export const selectMovies = state => selectPopularMovies(state).movies;
+export const selectPage = state => selectPopularMovies(state).page;
+export const selectTotalPages = state => selectPopularMovies(state).totalPages;
+export const selectTotalResults = state => selectPopularMovies(state).totalResults;
+export const selectLoading = state => selectPopularMovies(state).loading;
+export const selectError = state => selectPopularMovies(state).error;
+
+export const popularMoviesReducer = moviesSlice.reducer;
