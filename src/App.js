b34466040c1ec8dfea35b4966fc0886db0c68fd7
Robert Osial <rob.osial@gmail.com>
2022-03-11 15:29:21 +0000

    added PersonList Component to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Header } from "./common/Header";
 import { NoResultsPage } from "./common/NoResultsPage";
 import { MovieList } from "./features/movies/MovieList";
+import { PersonList } from "./features/people/PersonList";
 
 function App() {
   return (
     <>
       <Header />
       <MovieList />
+      {/* <PersonList /> */}
       {/* <NoResultsPage /> */}
     </>
   );
