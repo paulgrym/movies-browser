f48e009899a371e392e822757c408823de5f6f30
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-03-12 18:04:58 +0100

    state error added to MovieList

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -20,25 +20,28 @@ export const MovieList = () => {
 
   return (
     <Section>
-      <Title>Popular movies</Title>
-      {loading
-        ? <Loader />
-        : error
-          ? "Error"
-          : <Wrapper>
-            {
-              popularMovies.map((movie, index) => (
-                <MovieTile
-                  key={index}
-                  poster={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                  title={movie.title}
-                  date={movie.release_date.slice(0, 4)}
-                  voteAverage={movie.vote_average}
-                  voteCount={`${movie.vote_count} votes`}
-                />
-              ))
-            }
-          </Wrapper>
+      {error
+        ? "Error"
+        : <>
+          <Title>Popular movies</Title>
+          {loading
+            ? <Loader />
+            : <Wrapper>
+              {
+                popularMovies.map((movie, index) => (
+                  <MovieTile
+                    key={index}
+                    poster={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    title={movie.title}
+                    date={movie.release_date.slice(0, 4)}
+                    voteAverage={movie.vote_average}
+                    voteCount={`${movie.vote_count} votes`}
+                  />
+                ))
+              }
+            </Wrapper>
+          }
+        </>
       }
     </Section>
   );
