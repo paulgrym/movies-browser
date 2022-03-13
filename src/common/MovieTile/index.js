0099ea03c15c68dc4d536e5df99ed21914391725
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-13 12:33:55 +0100

    MovieTile component moves to common folder

---
+++ src/common/MovieTile/index.js
@@ -0,0 +1,43 @@
+import {
+  MovieTileContent,
+  MovieTileIcon,
+  MovieTileImage,
+  MovieTileRate,
+  MovieTileRating,
+  MovieTileSubtitle,
+  MovieTileTag,
+  MovieTileTags,
+  MovieTileTitle,
+  MovieTileVotes,
+  StyledMovieTile,
+  NoMovieImage,
+  NoMovieImageIcon,
+} from "./styled";
+
+export const MovieTile = ({ poster, title, date, voteAverage, voteCount }) => {
+
+  return (
+    <StyledMovieTile>
+      <MovieTileImage src={poster} alt="" />
+      {/* <NoMovieImage><NoMovieImageIcon /></NoMovieImage> */}
+
+      <MovieTileContent>
+        <MovieTileTitle> {title} </MovieTileTitle>
+        <MovieTileSubtitle>{date}</MovieTileSubtitle>
+
+        <MovieTileTags>
+          <MovieTileTag> Action </MovieTileTag>
+          <MovieTileTag> Adventure </MovieTileTag>
+          <MovieTileTag> Drama </MovieTileTag>
+        </MovieTileTags>
+
+        <MovieTileRating>
+          <MovieTileIcon />
+          <MovieTileRate>{voteAverage} </MovieTileRate>
+          <MovieTileVotes>{voteCount}</MovieTileVotes>
+        </MovieTileRating>
+      </MovieTileContent>
+
+    </StyledMovieTile>
+  );
+};
