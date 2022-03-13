d6203acfb0156f02b9c57b9fb8547f9adfacb568
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-13 12:37:35 +0100

    changed track MovieTile component. Used ternary operator to handle ErrorPage component.

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -1,37 +1,46 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovies, selectMovies } from "../../popularMoviesSlice";
+import {
+  fetchMovies,
+  selectMovies,
+  selectError,
+} from "../../popularMoviesSlice";
+import { ErrorPage } from "../../../common/ErrorPage";
 import { Section } from "../../../common/Section";
-import { MovieTile } from "./MovieTile";
 import { Wrapper } from "./styled";
+import { MovieTile } from "../../../common/MovieTile";
 
 export const MovieList = () => {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-useEffect(() => {
-  dispatch(fetchMovies());
-},[dispatch])
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
 
-const popularMovies = useSelector(selectMovies);
-// const loading = useSelector(selectLoading);
-// const error = useSelector(selectError);
+  const popularMovies = useSelector(selectMovies);
+  // const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
 
   return (
-    <Section title={"Popular movies"}>
-      <Wrapper>
-        {
-          popularMovies.map((movie, index) => (
-            <MovieTile
-              key={index}
-              poster={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-              title={movie.title}
-              date={movie.release_date.slice(0, 4)}
-              voteAverage={movie.vote_average}
-              voteCount={`${movie.vote_count} votes`}
-            />
-          ))
-        }
-      </Wrapper>
-    </Section>
+    <>
+      {error ? (
+        <ErrorPage />
+      ) : (
+        <Section title={"Popular movies"}>
+          <Wrapper>
+            {popularMovies.map((movie, index) => (
+              <MovieTile
+                key={index}
+                poster={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                title={movie.title}
+                date={movie.release_date.slice(0, 4)}
+                voteAverage={movie.vote_average}
+                voteCount={`${movie.vote_count} votes`}
+              />
+            ))}
+          </Wrapper>
+        </Section>
+      )}
+    </>
   );
 };
