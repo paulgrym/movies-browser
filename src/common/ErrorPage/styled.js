5b7709541926770c578fa95713197daf8cde4699
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-13 15:10:53 +0100

    added new media properties

--- src/common/ErrorPage/styled.js
+++ src/common/ErrorPage/styled.js
@@ -5,23 +5,29 @@ export const Wrapper = styled.div`
   text-align: center;
   margin-top: 180px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
-    margin-top: 40px;
-    padding: 20px;
-  }
-
   @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
-    margin-top: 60px;
+    margin-top: 100px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    margin-top: 40px;
+    padding: 16px;
   }
 `;
 
 export const Icon = styled(ErrorIcon)`
   margin-bottom: 38px;
+  width: 120px;
+  height: 120px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
-   width: 60px;
-   height: 60px;
-   margin-bottom: 20px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    width: 90px;
+    height: 90px;
+    margin-bottom: 20px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    width: 60px;
+    height: 60px;
+    margin-bottom: 20px;
   }
 `;
 
@@ -31,8 +37,11 @@ export const Info = styled.h2`
   line-height: 1.2;
   margin: 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
-   font-size: 20px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    font-size: 28px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 20px;
   }
 `;
 
@@ -42,7 +51,10 @@ export const Message = styled.p`
   line-height: 1.3;
   margin: 24px 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    font-size: 20px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
     font-size: 16px;
   }
 `;
@@ -53,10 +65,25 @@ export const Button = styled.button`
   font-weight: 700;
   color: ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.blue};
+  border: none;
   border-radius: 5px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+  :hover {
+    transform: scale(1.1);
+    transition: 0.5s;
+    cursor: pointer;
+  }
+
+  :active {
+    filter: brightness(140%);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
     padding: 12px 16px;
+    font-size: 12px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    padding: 8px 12px;
     font-size: 12px;
   }
 `;
