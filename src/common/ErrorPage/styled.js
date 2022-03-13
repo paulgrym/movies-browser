8accf48d86a4da8f59ba6b3fbb5bb1efd1c8854f
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-13 13:27:58 +0100

    media with styles added for mobiles

--- src/common/ErrorPage/styled.js
+++ src/common/ErrorPage/styled.js
@@ -4,10 +4,25 @@ import { ReactComponent as ErrorIcon } from "../../images/error.svg";
 export const Wrapper = styled.div`
   text-align: center;
   margin-top: 180px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+    margin-top: 40px;
+    padding: 20px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    margin-top: 60px;
+  }
 `;
 
 export const Icon = styled(ErrorIcon)`
   margin-bottom: 38px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+   width: 60px;
+   height: 60px;
+   margin-bottom: 20px;
+  }
 `;
 
 export const Info = styled.h2`
@@ -15,6 +30,10 @@ export const Info = styled.h2`
   font-weight: 600;
   line-height: 1.2;
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+   font-size: 20px;
+  }
 `;
 
 export const Message = styled.p`
@@ -22,6 +41,10 @@ export const Message = styled.p`
   font-weight: 500;
   line-height: 1.3;
   margin: 24px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+    font-size: 16px;
+  }
 `;
 
 export const Button = styled.button`
@@ -31,4 +54,9 @@ export const Button = styled.button`
   color: ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.blue};
   border-radius: 5px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+    padding: 12px 16px;
+    font-size: 12px;
+  }
 `;
