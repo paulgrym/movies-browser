e349bda1001bd84110be077436d926edcbc5ca84
Robert Osial <rob.osial@gmail.com>
2022-03-14 10:58:28 +0000

    changed font-weight to 500, changed line-height to 1.3, fixed radius on the photo,

--- src/features/people/PersonList/PersonTile/styled.js
+++ src/features/people/PersonList/PersonTile/styled.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Wrapper = styled.article`
   background: ${({ theme }) => theme.colors.white};
-  min-height: 339px;
+  padding: 10px 16px;
+  height: 100%;
   display: grid;
   grid-template-rows: auto 1fr;
   box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
@@ -15,27 +16,24 @@ export const Wrapper = styled.article`
 
   @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
     min-height: 245px;
-  }
-`;
-
-export const PersonImage = styled.img`
-  padding: 10px 16px;
-  height: 286px;
-  border-radius: 5px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
-    height: 196px;
     padding: 8px;
   }
 `;
 
+export const PersonImage = styled.img`
+  width: 100%;
+  border-radius: 5px;
+`;
+
 export const PersonFullName = styled.p`
-  margin: 0;
+  margin: 12px 0 24px 0;
   display: flex;
   justify-content: center;
   text-align: center;
   font-size: 22px;
-  
+  font-weight: 500;
+  line-height: 1.3;
+
   @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
     font-size: 16px;
   }
