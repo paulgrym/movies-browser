86d0bbe8ffcb11dc0d3bd885cffa95cfa0faef83
Robert Osial <rob.osial@gmail.com>
2022-03-14 10:54:26 +0000

    changed grid-grap to 24px on bigger screens and to 16px on mobile

--- src/features/people/PersonList/styled.js
+++ src/features/people/PersonList/styled.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, 208px);
-  grid-gap: 16px;
+  grid-gap: 24px;
   justify-content: center;
   margin: 12px auto 40px;
 
 
   @media (max-width: ${({ theme }) => theme.breakpoints.small}){
     grid-template-columns: repeat(auto-fill, 136px);
+    grid-gap: 16px;
   }
   `;
 
