6042d082431b04b2e3b03be27d2e485120059412
Robert Osial <rob.osial@gmail.com>
2022-03-15 20:58:04 +0000

    added watchFetchPopularPeople to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,6 +1,10 @@
 import { all } from "redux-saga/effects";
 import { watchFetchPopularMovies } from "./features/movies/MovieList/popularMoviesSaga";
+import { watchFetchPopularPeople } from "./features/people/PersonList/popularPeopleSaga";
 
 export default function* rootSaga() {
-  yield all([watchFetchPopularMovies()]);
+  yield all([
+    watchFetchPopularMovies(),
+    watchFetchPopularPeople(),
+  ]);
 };
