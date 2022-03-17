4442b6c23fe399cdc2c9d6e2709419d31efa1174
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-17 16:09:23 +0100

    changed logic in ternary operator, used query and movies selector

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovies, selectError, selectLoading, selectMovies } from "./popularMoviesSlice";
+import {
+  fetchMovies,
+  selectError,
+  selectLoading,
+  selectMovieByQuery,
+} from "./popularMoviesSlice";
 import { Section } from "../../../common/Section";
 import { Wrapper } from "./styled";
 import { Title } from "../../../common/Title";
@@ -8,29 +13,50 @@ import { Loader } from "../../../common/Loader";
 import { ErrorPage } from "../../../common/ErrorPage";
 import { MovieTile } from "../../../common/MovieTile";
 import { APIImageUrl } from "../../APIdata";
+import searchQueryParamName from "../../../common/Search/searchQueryParamName";
+import { NoResultsPage } from "../../../common/NoResultsPage";
+import { useQueryParameter } from "../../../common/Search/queryParameterHooks";
 
 export const MovieList = () => {
   const dispatch = useDispatch();
+  const query = useQueryParameter(searchQueryParamName);
 
   useEffect(() => {
     dispatch(fetchMovies());
-  }, [dispatch])
+  }, [dispatch, query]);
 
-  const popularMovies = useSelector(selectMovies);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
+  const movies = useSelector((state) => selectMovieByQuery(state, query));
 
   return (
-    <Section>
-      {error
-        ? <ErrorPage />
-        : <>
-          <Title>Popular movies</Title>
-          {loading
-            ? <Loader />
-            : <Wrapper>
-              {
-                popularMovies.map((movie, index) => (
+      <Section>
+      {query && movies.length === 0
+        ? <NoResultsPage query={query}/>
+        :
+      loading
+            ?
+            <>
+              <Title>
+                {query
+                  ? `Search results for "${query[0].toUpperCase() + query.slice(1)}"`
+                  : "Popular movies"
+                }
+              </Title>
+              <Loader />
+            </>
+            : error
+              ? <ErrorPage />
+              :
+              <>
+                <Title>
+                  {query
+                    ? `Search results for "${query[0].toUpperCase() + query.slice(1)}" (${movies.length})`
+                    : "Popular movies"
+                  }
+                </Title>
+                <Wrapper>
+                {movies.map((movie, index) => (
                   <MovieTile
                     key={index}
                     poster={`${APIImageUrl}/w342${movie.poster_path}`}
@@ -40,12 +66,10 @@ export const MovieList = () => {
                     voteAverage={movie.vote_average}
                     voteCount={`${movie.vote_count} votes`}
                   />
-                ))
+                ))}
+              </Wrapper>
+                </>
               }
-            </Wrapper>
-          }
-        </>
-      }
-    </Section>
+      </Section>
   );
 };
