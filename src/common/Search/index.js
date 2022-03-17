40b535a6d8ef08a19c3adedf3c0036dab1d69a3e
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-17 15:53:26 +0100

    handled Search component, created onInputChange function and hook useReplaceQueryParamteter used.

---
+++ src/common/Search/index.js
@@ -0,0 +1,28 @@
+
+import debounce from 'lodash.debounce';
+import {useLocation} from "react-router-dom";
+import { StyledSearch } from "./styled";
+import searchQueryParamName from "./searchQueryParamName";
+import { useReplaceQueryParameter } from "./queryParameterHooks";
+
+export const Search = () => {
+  const location = useLocation();
+  const replaceQueryParameter = useReplaceQueryParameter();
+
+const onInputChange = debounce(({ target }) => {
+  replaceQueryParameter({
+    key: searchQueryParamName,
+    value: target.value.trim() !== "" ? target.value : undefined,
+  });
+
+  target.value = "";
+}, 1000)
+
+  return <StyledSearch 
+           onChange={onInputChange}
+           placeholder={`Search for ${location.pathname === "/movies"
+            ?  "movies..."
+            :  "people..."}
+          `}
+          />;
+};
