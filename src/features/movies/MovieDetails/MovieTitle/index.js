2fa6999d1367f6828627da9718fa160d3d075e4f
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-03-23 00:27:40 +0100

    added Rating in MovieTitle

--- src/features/movies/MovieDetails/MovieTitle/index.js
+++ src/features/movies/MovieDetails/MovieTitle/index.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectMovie } from "../movieDetailsSlice";
-import { Title, Wrapper } from "./styled";
+import { MaxRate, Rate, RateContainer, RateIcon, Rating, Title, Votes, Wrapper } from "./styled";
 
 export const MovieTitle = () => {
   const movieDetails = useSelector(selectMovie);
@@ -8,6 +8,14 @@ export const MovieTitle = () => {
   return (
     <Wrapper>
       <Title>{movieDetails.title}</Title>
+      <Rating>
+        <RateContainer>
+          <RateIcon />
+          <Rate>{movieDetails.vote_average} </Rate>
+          <MaxRate>/10 </MaxRate>
+        </RateContainer>
+        <Votes>{movieDetails.vote_count} votes</Votes>
+      </Rating>
     </Wrapper>
   );
 };
--- src/features/movies/MovieDetails/MovieTitle/styled.js
+++ src/features/movies/MovieDetails/MovieTitle/styled.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { ReactComponent as StarIcon } from "../../../../images/star.svg";
+
 
 export const Wrapper = styled.div`
   margin-bottom: 64px;
@@ -26,3 +28,92 @@ export const Title = styled.h2`
     font-size: 24px;
   }
 `
+
+export const Rating = styled.div`
+  margin: 24px 0 56px;
+  font-size: 16px;
+  line-height: 1.2;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}){
+    margin: 8px 0 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}){
+    font-size: 10px;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-bottom: 8px;
+    gap: 8px;
+  }
+`;
+
+export const RateContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}){
+    margin-bottom: 0;
+
+  }
+`
+
+export const RateIcon = styled(StarIcon)`
+  color: ${({ theme }) => theme.colors.yellow};
+  height: auto;
+  width: 40px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}){
+    width: 30px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}){
+    width: 16px;
+  }
+`;
+
+export const Rate = styled.span`
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 1.3;
+  margin: 0 8px;
+
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}){
+    font-size: 18px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}){
+    font-weight: 600;
+    font-size: 14px;
+    margin: 0 2px 0 4px;
+  }
+`;
+
+export const MaxRate = styled.span`
+  font-size: 16px;
+  line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}){
+    font-size: 12px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}){
+    font-size: 10px;
+  }
+`
+
+export const Votes = styled.p`
+  font-size: 16px;
+  line-height: 1.2;
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}){
+    font-size: 12px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}){
+    font-size: 10px;
+  }
+`;
