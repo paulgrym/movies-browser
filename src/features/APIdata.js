a775ec51a4b730e2ed096cd271ae74316fb1edeb
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-22 17:58:16 +0100

    url from search movie api aded

--- src/features/APIdata.js
+++ src/features/APIdata.js
@@ -2,6 +2,8 @@ export const APIUrl = "https://api.themoviedb.org/3"
 
 export const APIKey = "951446c9e47a99cb6ccfb776c60023c7"
 
+export const APISearchMovieUrl = `${APIUrl}/search/movie?api_key=${APIKey}&query=`
+
 export const APIPopularMoviesUrl = `${APIUrl}/movie/popular?api_key=${APIKey}`
 
 export const APIPopularPeopleUrl = `${APIUrl}/person/popular?api_key=${APIKey}`
