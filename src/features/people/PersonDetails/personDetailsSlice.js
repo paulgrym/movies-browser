0cff427248fe6541eefee1d7dc096c7621584d09
Robert Osial <rob.osial@gmail.com>
2022-03-23 23:25:41 +0000

    created personDetailsSlice

---
+++ src/features/people/PersonDetails/personDetailsSlice.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const personDetailsSlice = createSlice({
+  name: "personDetails",
+  initialState: {
+    personDetails: [],
+    cast: [],
+    crew: [],
+    loading: false,
+    error: false,
+  },
+
+  reducers: {
+    fetchPersonDetails: state => {
+      state.loading = true;
+    },
+
+    fetchPersonDetailsSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.error = false;
+      state.personDetails = payload;
+    },
+
+    fetchPersonDetailsError: state => {
+      state.error = true;
+    },
+
+    setMovies: (state, { payload }) => {
+      state.cast = payload.cast;
+      state.crew = payload.crew;
+    },
+  },
+});
+
+export const {
+  fetchPersonDetails,
+  fetchPersonDetailsSuccess,
+  fetchPersonDetailsError,
+  setMovies,
+} = personDetailsSlice.actions;
+
+const selectPersonDetails = state => state.personDetails;
+
+export const selectPerson = state => selectPersonDetails(state).personDetails;
+export const selectMovieCast = state => selectPersonDetails(state).cast;
+export const selectMovieCrew = state => selectPersonDetails(state).crew;
+export const selectLoading = state => selectPersonDetails(state).loading;
+export const selectError = state => selectPersonDetails(state).error;
+
+export const personDetailsReducer = personDetailsSlice.reducer;
