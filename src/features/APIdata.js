d0758920f07982b1e30737af6a70994dbe21a336
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-23 12:57:23 +0100

    added apiUrl to search movies and persons

--- src/features/APIdata.js
+++ src/features/APIdata.js
@@ -2,11 +2,13 @@ export const APIUrl = "https://api.themoviedb.org/3"
 
 export const APIKey = "951446c9e47a99cb6ccfb776c60023c7"
 
-export const APISearchMoviesUrl = `${APIUrl}/search/movie?api_key=${APIKey}&query=`
-
 export const APIPopularMoviesUrl = `${APIUrl}/movie/popular?api_key=${APIKey}`
 
+export const APISearchMovieUrl = `${APIUrl}/search/movie?api_key=${APIKey}`
+
 export const APIPopularPeopleUrl = `${APIUrl}/person/popular?api_key=${APIKey}`
+
+export const APISearchPersonUrl = `${APIUrl}/search/person?api_key=${APIKey}`
 
 export const APIImageUrl = `https://image.tmdb.org/t/p`
 
