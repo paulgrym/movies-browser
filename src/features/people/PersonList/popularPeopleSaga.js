8a6e96461658e657ac3c0d52ce78e4c3b1489209
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-03-23 13:07:51 +0100

    used searchPersonUrl, handled search person by query

--- src/features/people/PersonList/popularPeopleSaga.js
+++ src/features/people/PersonList/popularPeopleSaga.js
@@ -1,20 +1,27 @@
 import { delay, call, put, takeLatest } from "redux-saga/effects";
-import { APIPopularPeopleUrl } from "../../APIdata";
+import { APIPopularPeopleUrl, APISearchPersonUrl } from "../../APIdata";
 import { getAPI } from "../../getAPI";
-import { fetchPeople, fetchPeopleError, fetchPeopleSuccess } from "./popularPeopleSlice";
+import {
+  fetchPeople,
+  fetchPeopleError,
+  fetchPeopleSuccess
+} from "./popularPeopleSlice";
 
-function* fetchPopularPeopleWorker() {
-  const popularPeoplePage1 = `${APIPopularPeopleUrl}&page=1`;
+function* fetchPopularPeopleWorker( { payload: { query } }) {
+  const popularPeople = `${APIPopularPeopleUrl}&page=1`;
+  const searchPerson = `${APISearchPersonUrl}&query=${query}`;
+  const request = !query ? popularPeople : searchPerson;
 
   try {
-    yield delay(2000);
-    const popularPeople = yield call(getAPI, popularPeoplePage1);
-    yield put(fetchPeopleSuccess(popularPeople));
+    yield delay(1000);
+    const requestPeople = yield call(getAPI, request);
+    console.log(requestPeople);
+    yield put(fetchPeopleSuccess(requestPeople));
   } catch (error) {
     yield put(fetchPeopleError());
-  };
-};
+  }
+}
 
 export function* watchFetchPopularPeople() {
   yield takeLatest(fetchPeople.type, fetchPopularPeopleWorker);
-};
+}
