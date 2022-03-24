681d197348a83fc843df1dd463dff98ac7b137e5
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-03-24 11:42:09 +0100

    condition displaying cast or crew if they exist

--- src/features/movies/MovieDetails/Cast/index.js
+++ src/features/movies/MovieDetails/Cast/index.js
@@ -1,11 +1,11 @@
 import { nanoid } from "@reduxjs/toolkit";
 import { useState } from "react";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import { PersonTile } from "../../../../common/PersonTile";
 import { Section } from "../../../../common/Section";
 import { Title } from "../../../../common/Title";
 import { APIImageUrl } from "../../../APIdata";
-import { selectMovieCast } from "../movieDetailsSlice"
+import { selectMovieCast } from "../movieDetailsSlice";
 import { Arrow, Container, ShowAllButton } from "../../../../common/ShowAllButton";
 import { PeopleWrapper } from "../../../../common/PeopleWrapper";
 
@@ -18,34 +18,32 @@ export const Cast = () => {
 
   const castShown = isAllCastShown ? cast.length : 10;
 
-  return (
-    <Section as="article">
-      <Title>Cast</Title>
-
-      <PeopleWrapper>
-        {cast.slice(0, castShown).map(person => (
-          <PersonTile
-            key={nanoid()}
-            profileImage={`${APIImageUrl}/w185${person.profile_path}`}
-            profilePath={person.profile_path}
-            fullName={person.name}
-            character={person.character}
-          />
-        ))
-        }
-      </PeopleWrapper>
-
-      {cast.length >= 10 && <Container>
-        <ShowAllButton
-          onClick={toggleIsAllCastShown}
-          isAllPeopleShown={isAllCastShown}
-        >
-          {isAllCastShown && <Arrow> ⇑ </Arrow>}
-          {isAllCastShown ? "Hide" : "Show all"}
-          {!isAllCastShown && <Arrow> ⇓</Arrow>}
-        </ShowAllButton>
-      </Container>}
-
-    </Section>
-  );
+  if (cast.length !== 0) {
+    return (
+      <Section as="article">
+        <Title>Cast</Title>
+        <PeopleWrapper>
+          {cast.slice(0, castShown).map(person => (
+            <PersonTile
+              key={nanoid()}
+              profileImage={`${APIImageUrl}/w185${person.profile_path}`}
+              profilePath={person.profile_path}
+              fullName={person.name}
+              character={person.character}
+            />
+          ))}
+        </PeopleWrapper>
+        {cast.length >= 10 && <Container>
+          <ShowAllButton
+            onClick={toggleIsAllCastShown}
+            isAllPeopleShown={isAllCastShown}
+          >
+            {isAllCastShown && <Arrow> ⇑ </Arrow>}
+            {isAllCastShown ? "Hide" : "Show all"}
+            {!isAllCastShown && <Arrow> ⇓</Arrow>}
+          </ShowAllButton>
+        </Container>}
+      </Section>
+    );
+  } else return null;
 };
